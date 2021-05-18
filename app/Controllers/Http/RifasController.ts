8776aadea107fb2530d8fe63744b3754875ce136
Gabriel Crisanto <gabrecrisanto@gmail.com>
2021-05-18 16:06:26 -0300

    autenticacao

--- app/Controllers/Http/RifasController.ts
+++ app/Controllers/Http/RifasController.ts
@@ -8,22 +8,4 @@ export default class RifasController {
     public async show({ view }: HttpContextContract) {
         return view.render('rifa/show')
     }
-
-    public async create({}: HttpContextContract) {
-    }
-
-    public async store({}: HttpContextContract) {
-    }
-
-    public async done({}: HttpContextContract) {
-    }
-
-    public async edit({}: HttpContextContract) {
-    }
-
-    public async update({}: HttpContextContract) {
-    }
-
-    public async destroy({}: HttpContextContract) {
-    }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', 'RifasController.index')
+Route.get('/', 'RifasController.index').as('root')
 Route.get('/about', 'RifasController.show')
+
+Route.get('/register', 'AuthController.register').as('auth.register')
+Route.post('/register', 'AuthController.store').as('auth.store')
+Route.get('/login', 'AuthController.login').as('auth.login')
+Route.post('/login', 'AuthController.verify').as('auth.verify')
+Route.get('/logout', 'AuthController.logout').as('auth.logout')
